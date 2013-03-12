18ea61f9c06813c8a5966942bbfbb84a22336c9d
Vasilica Victor <vasilica.victor@gmail.com>
2013-03-12 23:03:02 +0200

    update on script

--- lab#1/makeScript.rb
+++ lab#1/makeScript.rb
@@ -1,13 +1,23 @@
-#A small script to compile a few files in the directory where it is
+#A small script to compile a few files in its  directory.
+
+#To complete all the task reqiered in the laboratory work I am using 
+#a beautiful ruby gem "mail" which can be found here 
+#https://github.com/mikel/mail.giti
+
+require 'mail'
+
 #########################
 
 #begginig message
 system 'echo "Starting"'
 
+message = "success message"
 ##compiling the .cpp file
 print "Compiling the .cpp file . . ." 
-system 'echo "$(g++ -Wall -W -Werror hello.cpp -o helloCPP)"'
-puts "[done]"
+if  system 'echo "$(g++ -Wall -W -Werror hello.cpp -o helloCPP)"'
+        system 'echo "$(git commit -m "success message")"'
+        puts "[done]"
+end     	
 
 ##compiling the .c fil
 print "Compiling the .c file . . ." 
@@ -36,3 +46,21 @@ system 'echo "$(ruby hello.rb)"'
 
 puts "Running the .py executable"
 system 'echo "$(python hello.py)"'
+
+
+
+
+
+
+mail = Mail.new do
+  from     'me@gmail.com'
+  to       'vasilica.victor@gmail.com'
+  subject  'Here is the image you wanted'
+  body     File.read('body.txt')
+end
+
+mail.delivery_method :sendmail
+
+mail.deliver
+
+puts "Script [complete]"
