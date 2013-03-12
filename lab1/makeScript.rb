a3ea733080f2490207f9161d7491323d4b3cc517
Vasilica Victor <vasilica.victor@gmail.com>
2013-03-12 16:01:05 +0200

    updated the script

--- lab1/makeScript.rb
+++ lab1/makeScript.rb
@@ -1,7 +1,47 @@
+#A small script to compile a few files in the directory where it is
+#######################
 
-
+#begginig message
 system 'echo "Starting the compilation"'
-system 'echo "$(g++ -Wall -W -Werror hello.cpp -o helloCPP)"'
 
+#compiling the .cpp file
+print "Compiling the .cpp file . . ." 
+system 'echo "$(g++ -Wall -W -Werror hello.cpp -o helloCPP)"'
+puts "[done]"
+
+#compiling the .c fil
+print "Compiling the .c file . . ." 
+system 'echo "$(gcc -Wall -W -Werror hello.c -o helloC)"'
+puts "[done]"
+
+#compiling the .java fil
+print "Compiling the .java file . . ." 
+system 'echo "$(javac hello.java)"'
+puts "[done]"
+
+#compiling the .ruby fil
+print "Compiling the .c file . . ." 
+system 'echo "$(ruby hello.rb)"'
+puts "[done]"
+
+#compiling the .py fil
+print "Compiling the .c file . . ." 
+system 'echo "$(python hello.py)"'
+puts "[done]"
+
+#run the executables from compilation
+
+puts "Running the .cpp executable"
 system 'echo "$(./helloCPP)"'
 
+puts "Running the .c executable"
+system 'echo "$(./helloC)"'
+
+puts "Running the .java executable"
+system 'echo "$(./HelloWorld.class)"'
+
+puts "Running the .rb executable"
+system 'echo "$(ruby hello.rb)"'
+
+puts "Running the .py executable"
+system 'echo "$(python hello.py)"'
